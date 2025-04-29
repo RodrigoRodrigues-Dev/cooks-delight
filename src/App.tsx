6525c91b447ee3beefbc9856a44a1925b1079d9e
Rodrigo Rodrigues <rs9844660@gmail.com>
2025-04-28 21:30:09 -0300

    feat(app): add root App component integrating layout, routing outlet, Header, MobileMenu, CTASection and Footer

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,26 @@
+import { useState } from 'react';
+import { Outlet } from 'react-router-dom';
+import Container from './components/LayoutContainer/styled';
+import MenuMobile from './components/MobileMenu';
+import Header from './containers/Header';
+import Footer from './containers/Footer';
+import CTASection from './containers/CallToActionSection';
+
 function App() {
-  return <h1>Hello React</h1>;
+  const [menuVisible, setMenuVisible] = useState(false);
+
+  return (
+    <Container>
+      <Header toggleMenuMobile={() => setMenuVisible(!menuVisible)} />
+      <MenuMobile
+        onClose={() => setMenuVisible(false)}
+        $menuVisible={menuVisible}
+      />
+      <Outlet />
+      <CTASection></CTASection>
+      <Footer />
+    </Container>
+  );
 }
 
 export default App;

6c365d1f556039c3313bfcd48256f88086134cea
Rodrigo Rodrigues <rs9844660@gmail.com>
2025-05-09 10:15:40 -0300

    feat(about-us-section): add AboutUsSection container with tag, title, paragraph and images

---
+++ src/containers/AboutUsSection/index.tsx
@@ -0,0 +1,32 @@
+import Button from '../../components/Button';
+import Tag from '../../components/Tag';
+import Title from '../../components/Title';
+
+import {
+  AboutUsWrapper,
+  TextWrapper,
+  Image1,
+  Image2,
+  Image3,
+  ParagraphAbout
+} from './styled';
+
+const AboutUsSection = () => (
+  <AboutUsWrapper>
+    <TextWrapper>
+      <Tag>About Us</Tag>
+      <Title $type="Medium">Our Culinary Chronicle</Title>
+      <ParagraphAbout $type="secondary" $size={16}>
+        Our journey is crafted with dedication, creativity, and an unrelenting
+        commitment to delivering delightful culinary experiences. Join us in
+        savoring the essence of every dish and the stories that unfold.
+      </ParagraphAbout>
+      <Button>Read more</Button>
+    </TextWrapper>
+    <Image1 />
+    <Image2 />
+    <Image3 />
+  </AboutUsWrapper>
+);
+
+export default AboutUsSection;

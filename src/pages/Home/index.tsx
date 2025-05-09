06b826b8827368e119100df9ee0db519e307aa83
Rodrigo Rodrigues <rs9844660@gmail.com>
2025-05-09 10:17:25 -0300

    feat(home): add Home page composing Hero, Explore, Featured, Recipes and About Us sections

---
+++ src/pages/Home/index.tsx
@@ -0,0 +1,17 @@
+import AboutUsSection from '../../containers/AboutUsSection';
+import ExploreSection from '../../containers/ExploreSection';
+import FeaturedSection from '../../containers/FeaturedSection';
+import HeroSection from '../../containers/HeroSection';
+import RecipesSection from '../../containers/RecipesSection';
+
+const Home = () => (
+  <>
+    <HeroSection />
+    <ExploreSection />
+    <FeaturedSection />
+    <RecipesSection />
+    <AboutUsSection />
+  </>
+);
+
+export default Home;

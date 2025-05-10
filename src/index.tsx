0422e2c8058203b13905d72986c308cb7645f765
Rodrigo Rodrigues <rs9844660@gmail.com>
2025-05-10 11:09:37 -0300

    feat(router): add /recipe/:id route for Recipe page in router configuration

--- src/index.tsx
+++ src/index.tsx
@@ -5,6 +5,7 @@ import reportWebVitals from './reportWebVitals';
 import { Reset } from './styles/GlobalStyles';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import Home from './pages/Home';
+import Recipe from './pages/Recipe';
 
 const router = createBrowserRouter([
   {
@@ -14,6 +15,10 @@ const router = createBrowserRouter([
       {
         path: '/',
         element: <Home />
+      },
+      {
+        path: '/recipe/:id',
+        element: <Recipe />
       }
     ]
   }

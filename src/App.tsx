287264b6170183032b3da94a9a17582ebf6d6dc8
Rodrigo Rodrigues <rs9844660@gmail.com>
2025-05-12 13:08:09 -0300

    feat(app): prevent background scroll and scroll to top when search box or mobile menu opens

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,47 @@
-import { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { Outlet } from 'react-router-dom';
+
+import { useSelector } from 'react-redux';
+import type { RootState } from './store';
+
 import Container from './components/LayoutContainer/styled';
 import MenuMobile from './components/MobileMenu';
+
 import Header from './containers/Header';
-import Footer from './containers/Footer';
+import SearchBox from './containers/SearchBox';
 import CTASection from './containers/CallToActionSection';
-import { Provider } from 'react-redux';
-import { store } from './store';
+import Footer from './containers/Footer';
 
 function App() {
   const [menuVisible, setMenuVisible] = useState(false);
+  const searchBoxVisibility = useSelector(
+    (state: RootState) => state.searchBoxVisibility.isVisible
+  );
+
+  useEffect(() => {
+    if (searchBoxVisibility || menuVisible) {
+      if (searchBoxVisibility) {
+        setMenuVisible(false);
+      }
+      document.body.style.overflow = 'hidden';
+      window.scrollTo(0, 0);
+    } else {
+      document.body.style.overflow = '';
+    }
+  }, [searchBoxVisibility, menuVisible]);
 
   return (
-    <Provider store={store}>
-      <Container>
-        <Header toggleMenuMobile={() => setMenuVisible(!menuVisible)} />
-        <MenuMobile
-          onClose={() => setMenuVisible(false)}
-          $menuVisible={menuVisible}
-        />
-        <Outlet />
-        <CTASection></CTASection>
-        <Footer />
-      </Container>
-    </Provider>
+    <Container>
+      <Header toggleMenuMobile={() => setMenuVisible(!menuVisible)} />
+      <MenuMobile
+        onClose={() => setMenuVisible(false)}
+        $menuVisible={menuVisible}
+      />
+      <SearchBox />
+      <Outlet />
+      <CTASection />
+      <Footer />
+    </Container>
   );
 }
 

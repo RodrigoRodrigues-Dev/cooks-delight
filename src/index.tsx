661a7e1d224255d2d329a14541c106c9ea3f135f
Rodrigo Rodrigues <rs9844660@gmail.com>
2025-05-12 09:34:20 -0300

    refactor(root): relocate Redux Provider from App component to index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,17 @@
+// index.tsx
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+
+import { Provider } from 'react-redux';
+import { store } from './store';
+
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+
+import { Reset } from './styles/GlobalStyles';
+
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { Reset } from './styles/GlobalStyles';
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+
 import Home from './pages/Home';
 import Recipe from './pages/Recipe';
 
@@ -12,14 +20,8 @@ const router = createBrowserRouter([
     path: '',
     element: <App />,
     children: [
-      {
-        path: '/',
-        element: <Home />
-      },
-      {
-        path: '/recipe/:id',
-        element: <Recipe />
-      }
+      { path: '/', element: <Home /> },
+      { path: '/recipe/:id', element: <Recipe /> }
     ]
   }
 ]);
@@ -27,14 +29,14 @@ const router = createBrowserRouter([
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
+
 root.render(
   <React.StrictMode>
-    <Reset />
-    <RouterProvider router={router} />
+    <Provider store={store}>
+      <Reset />
+      <RouterProvider router={router} />
+    </Provider>
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
